e4451e7c26b5d92113a17e820409cb65002003bd
Saavedra Sebastian <ssaavedraa4@upao.edu.pe>
2024-09-09 20:54:21 -0500

    Creación de entorno de API

---
+++ src/main/java/com/grupo5/findmindsetbackend/FindmindsetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grupo5.findmindsetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmindsetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindmindsetBackendApplication.class, args);
+    }
+
+}
